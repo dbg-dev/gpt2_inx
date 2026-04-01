e2bceb97e5183f898a5755ea5fbc0cb86b34f115
minky.raccoon <minky.raccoon@pm.me>
2026-04-01 10:21:25 +0100

    feat: add config for GPT2 355M

--- src/gpt2_inx/configs/hyperparams.py
+++ src/gpt2_inx/configs/hyperparams.py
@@ -9,3 +9,13 @@ GPT2_124M = hyparams(
     ff_hidden_dim=(4 * 768),  # 4 x em.
     use_bias=True,
 )
+
+GPT2_355M = hyparams(
+    vocab_size=50257,
+    embed_dim=1024,
+    num_heads=16,
+    num_layers=24,
+    ctx_len=1024,
+    ff_hidden_dim=(4 * 1024),  # 4 x em.
+    use_bias=True,
+)
