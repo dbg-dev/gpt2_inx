749d4a5e3c9e7dc91c652569da794e5529d9451d
minky.raccoon <minky.raccoon@pm.me>
2026-04-10 15:06:56 +0100

    chore: tidy inference

--- src/gpt2_inx/pipelines/inference.py
+++ src/gpt2_inx/pipelines/inference.py
@@ -1,6 +1,5 @@
 from typing import Any
-from gpt2_inx.utils import timeit
-from functools import partial 
+from functools import partial
 from flax.nnx import Module, GraphDef, GraphState, merge, split
 from jax import Array, jit
 from jax.numpy import int32, zeros, concat, where, bool_
@@ -12,27 +11,27 @@ from gpt2_inx.samplers import topk_sample
 
 EOS_TOKEN_ID = 50256
 
+
 @partial(jit, static_argnames=("graphdef", "k", "max_new_tokens"))
 def _generate_loop(
-    graphdef: GraphDef[Any], 
-    state: GraphState, 
-    prompt_ids: Array, 
-    k:int,                  # set sample size 
+    graphdef: GraphDef[Any],
+    state: GraphState,
+    prompt_ids: Array,
+    k: int,  # set sample size
     max_new_tokens: int,
-    key: int                # random split
+    key: int,  # random split
 ) -> tuple[Array, Any]:
     # preallocate a fixed-length buffer
     zeros_pad = zeros((1, max_new_tokens), dtype=int32)
     tokens = concat([prompt_ids, zeros_pad], axis=1)
 
-
     def step(carry, _):
         tokens, pos, key, done = carry
         key, subkey = rndm_split(key)
 
-        model  = merge(graphdef, state)
-        logits = model(tokens)                  # (1, max_len, vocab_size)
-        last_logits = logits[0, pos - 1, :]     # (vocab_size,)
+        model = merge(graphdef, state)
+        logits = model(tokens)  # (1, max_len, vocab_size)
+        last_logits = logits[0, pos - 1, :]  # (vocab_size,)
         next_token = topk_sample(last_logits, k=k, key=subkey)
 
         # If already done, emit EOS and don't update tokens
@@ -41,7 +40,6 @@ def _generate_loop(
 
         done = done | (next_token == EOS_TOKEN_ID)
         return (tokens, pos + 1, key, done), next_token
-
 
     start_pos = prompt_ids.shape[1]
     (tokens, _, _, _), new_tokens = scan(
@@ -54,8 +52,6 @@ def _generate_loop(
     return tokens, new_tokens
 
 
-
-@timeit
 def generate(
     model: Module, 
     prompt_ids: Array, 
